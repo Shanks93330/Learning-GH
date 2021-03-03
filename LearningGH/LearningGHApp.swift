7828d67b9c4722764d390ba5a296e19bd225c32d
Ludovic E <ebenemoussole2@yahoo.fr>
2021-03-03 21:21:59 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ludovic Ebene on 03/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
